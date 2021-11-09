a3f5d9981f8b0c6fc9f69d546d5b4b4c6a9a3fa3
Juan Sandoval <juan_icloud@hotmail.com>
2021-11-09 13:48:45 -0600

    nueva lambda

--- SFPasoTres/lambda_function.py
+++ SFPasoTres/lambda_function.py
@@ -1,33 +1,33 @@
 import json, boto3, decimal, os
 from datetime import datetime
-# client = boto3.client("lambda")
-# ConversationService = os.environ['ConversationService']
+client = boto3.client("lambda")
+ConversationService = os.environ['ConversationService']
 
 def lambda_handler(event, context):
-#    payload = {
-#       "operation": "update",
-#       "payload": {
-#             "Item": {
-#                "sessionId": event["sessionID"],
-#                "userMessage": event["mensaje"],
-#                "lastState": event["estado"],
-#                "intent": event["intent"],
-#                "lastQuestion": event["n_pregunta"],
-#                "botMessage": event["respuesta"]
-#             }
-#       }
-#    }
-#    response = client.invoke(
-#       FunctionName = ConversationService,
-#       InvocationType = 'RequestResponse',
-#       Payload = json.dumps(payload)
-#    )
-#    responseFromAPI = json.load(response['Payload'], parse_float=decimal.Decimal)
-#    print(responseFromAPI)
+   payload = {
+      "operation": "update",
+      "payload": {
+            "Item": {
+               "sessionId": event["sessionID"],
+               "userMessage": event["mensaje"],
+               "lastState": event["estado"],
+               "intent": event["intent"],
+               "lastQuestion": event["n_pregunta"],
+               "botMessage": event["respuesta"]
+            }
+      }
+   }
+   response = client.invoke(
+      FunctionName = ConversationService,
+      InvocationType = 'RequestResponse',
+      Payload = json.dumps(payload)
+   )
+   responseFromAPI = json.load(response['Payload'], parse_float=decimal.Decimal)
+   print(responseFromAPI)
    
-#    retornar = {"respuesta":event["respuesta"],"sessionID":event["sessionID"],"intent":event["intent"]}
-#    print("retornar ",retornar)
-   return #retornar
+   retornar = {"respuesta":event["respuesta"],"sessionID":event["sessionID"],"intent":event["intent"]}
+   print("retornar ",retornar)
+   return retornar
    
 def handle_decimal_type(obj):
   if isinstance(obj, Decimal):
