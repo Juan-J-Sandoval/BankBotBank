8af7f18fb629164a516b33cbd3af9b8357e8adb0
Juan Sandoval <juan_icloud@hotmail.com>
2021-11-17 19:20:40 -0600

    trainer v2

--- TrainerBot/lambda_function.py
+++ TrainerBot/lambda_function.py
@@ -1,4 +1,4 @@
-import boto3, json, os, io
+import boto3, json, os, io, yaml
 from snips_nlu import SnipsNLUEngine
 from snips_nlu.default_configs import CONFIG_ES
 bucket_name = os.getenv('MODEL_BUCKET_NAME', "")
@@ -7,10 +7,9 @@ s3 = boto3.resource('s3')
 
 bucket_name = os.environ['BUCKET_NAME']
 bot_data_file = os.environ['BOT_DATA_FILE']
-s3.download_file(bucket_name,bot_data_file,"/tmp/"+bot_data_file)
-f = open("/tmp/"+bot_data_file, "r")
-content = f.read()
-dataBotDS = json.loads(content)
+bot_name = os.environ['BOT_NAME']
+
+
 
 def lambda_handler(event, context):
     payload_dictionary = json.loads(json.dumps(event['payload']))
@@ -18,8 +17,8 @@ def lambda_handler(event, context):
 
     if operation == 'trainer':
         result = trainer(payload_dictionary)
-    elif operation=='retraining':
-        result = retraining(payload_dictionary)
+    elif operation=='getData':
+        result = getData(payload_dictionary)
     # print("THE BUCKET NAME IS: " + bucket_name)
     # yaml_path = "/tmp/"+ event['bot'] + ".yaml"
     # json_path = "/tmp/"+ event['bot'] + ".json"
@@ -43,19 +42,33 @@ def lambda_handler(event, context):
     # trained_model = nlu_engine.to_byte_array()
     # print("guardando modelo")
     # s3.meta.client.put_object(Bucket=bucket_name,Key=event['bot'] + "_model.json",Body=trained_model)
-    return {
-        "statusCode": 200,
-        "body": {"status": "ok"}
-    }
+    return result
 
-def retraining(payload):
+def trainer(payload):
     return {
         "statusCode": 200,
         "body":{"status": "reentrenado"}
     }
 
-def trainer(payload):
+def getData(payload):
+    #Snips
+    SnipsData=[]
+    s3.download_file(bucket_name,bot_name+'.yaml',"/tmp/"+bot_name+'.yaml')
+    with open("/tmp/"+bot_name+'.yaml') as f:
+        for data in yaml.load_all(f):
+            SnipsData.append(data)
+    #DistanciaSemantica
+    s3.download_file(bucket_name,bot_data_file,"/tmp/"+bot_data_file)
+    f = open("/tmp/"+bot_data_file, "r")
+    content = f.read()
+    dataBotDS = json.loads(content)
+    #Lex
+    s3.download_file(bucket_name,bot_name+'.json',"/tmp/"+bot_name+'.json')
+    f = open("/tmp/"+bot_name+'.json', "r")
+    content = f.read()
+    dataBotLex = json.loads(content)
+    response={"snips":SnipsData,"DS":dataBotDS['ResponseData'],"lex":dataBotLex}
     return {
         "statusCode": 200,
-        "body":{"status": "entrenado"}
+        "body":response
     }
