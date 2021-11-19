812ecc885228a8d84d6ede99ee2c85510e264fbb
Juan Sandoval <juan_icloud@hotmail.com>
2021-11-19 12:24:08 -0600

    trainer v6

--- TrainerBot/lambda_function.py
+++ TrainerBot/lambda_function.py
@@ -1,11 +1,13 @@
-import boto3, json, os, io, yaml
+import boto3, json, os, io, yaml, time
 from yaml import Loader
 from snips_nlu import SnipsNLUEngine
 from snips_nlu.default_configs import CONFIG_ES
-bucket_name = os.environ['BUCKET_NAME']
+
+lex = boto3.client('lex-models')
 nlu_engine = SnipsNLUEngine(config=CONFIG_ES)
 s3 = boto3.resource('s3')
 
+bucket_name = os.environ['BUCKET_NAME']
 bot_data_file = os.environ['BOT_DATA_FILE']
 bot_name = os.environ['BOT_NAME']
 
@@ -67,9 +69,40 @@ def getData(payload):
     f = open("/tmp/"+bot_name+'.json', "r")
     content = f.read()
     dataBotLex = json.loads(content)
-    #response={"snips":SnipsData,"DS":dataBotDS['ResponseData'],"lex":dataBotLex}
-    response={"snips":SnipsData}
+    response={"snips":SnipsData,"DS":dataBotDS['ResponseData'],"lex":dataBotLex['resource']}
     return {
         "statusCode": 200,
         "body":response
     }
+
+def lex(payload):
+    print('>>> ',payload)
+    #Extrae el zip del bucket 
+    s3Response = s3.get_object(Bucket=payload['bucket'],Key=payload['zip'])
+    dataB = s3Response['Body'].read()
+    #Se importa el zip a lex. Esto no lo construira, solo verifica que el zip lleve los archivos correctos
+    lexImport = lex.start_import(
+        payload=dataB,
+        resourceType='BOT',
+        mergeStrategy='OVERWRITE_LATEST'
+    )
+    #Se verifica que el estado de importación sea diferente a IN_PROGRESS
+    lexGetImport = lex.get_import(importId=lexImport['importId'])
+    while lexGetImport['importStatus'] == 'IN_PROGRESS':
+        time.sleep(15)
+        lexGetImport = lex.get_import(importId=lexImport['importId'])
+    #Se extraen las versiones del bot previamente importado, para despues extraer los datos de la version $LATEST junto con su Checksum
+    lexBotVersion= lex.get_bot_versions(name=lexImport['name'])
+    lexBot = lex.get_bot(name=lexBotVersion['bots'][0]['name'], versionOrAlias=lexBotVersion['bots'][0]['version'])
+    #Se reenvían los datos pero ahora se especifica el 'processBehavior' en BUILD para que el estado final del bot sea BUILDING
+    lexPutBot= lex.put_bot(
+        name=lexBot['name'],
+        locale=lexBot['locale'],
+        childDirected=lexBot['childDirected'],
+        checksum=lexBot['checksum'],
+        abortStatement = lexBot['abortStatement'],
+        intents= lexBot['intents'],
+        processBehavior='BUILD'
+    )
+    #Si se desea verificar que el bot está funcionando, se debe agregar un time sleep y volver a consultar con get_bot hasta que el estado sea READY
+    return {"NameBot":lexPutBot['name'], "Status":lexPutBot['status'], "Checksum":lexPutBot['checksum'], "Version":lexPutBot['version']}
