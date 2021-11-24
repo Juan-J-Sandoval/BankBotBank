a00af596895a3802ecd1262bf1a2e6be144f3330
Juan Sandoval <juan_icloud@hotmail.com>
2021-11-23 23:51:11 -0600

    trainer v6

--- TrainerBot/lambda_function.py
+++ TrainerBot/lambda_function.py
@@ -20,6 +20,111 @@ def lambda_handler(event, context):
         result = trainer(payload_dictionary)
     elif operation=='getData':
         result = getData()
+    return result
+
+def files_download():
+    #Snips
+    Snips=[]
+    s3.meta.client.download_file(bucket_name,bot_name+'.yaml',"/tmp/"+bot_name+'.yaml')
+    with open("/tmp/"+bot_name+'.yaml') as f:
+        for data in yaml.load_all(f, Loader=Loader):
+            Snips.append(data)
+    #DistanciaSemantica
+    s3.meta.client.download_file(bucket_name,bot_data_file,"/tmp/"+bot_data_file)
+    f = open("/tmp/"+bot_data_file, "r")
+    content = f.read()
+    ds = json.loads(content)
+    #Lex
+    s3.meta.client.download_file(bucket_name,bot_name+'.json',"/tmp/"+bot_name+'.json')
+    f = open("/tmp/"+bot_name+'.json', "r")
+    content = f.read()
+    Lex = json.loads(content)
+    print("Archivos descargados... /tmp/"+bot_name+".yaml /tmp/"+bot_data_file," /tmp/"+bot_name+".json")
+    return Snips, ds, Lex
+
+def files_upload(Snips, ds, Lex):
+    s3.meta.client.put_object(Bucket=bucket_name,Key=bot_data_file,Body=ds)
+    print("Archivos almacenados... ")
+    return True
+
+def trainer(payload):
+    SnipsData, dataBotDS, dataBotLex = files_download()
+    print("payload>> ",payload)
+    print("snips>> ",json.dumps(SnipsData))
+    print("DS>> ",json.dumps(dataBotDS))
+    print("lex>> ",json.dumps(dataBotLex))
+    #Se actualizan datos
+    dataBotLex["resource"]["intents"].clear()
+    dataBotDS["ResponseData"].clear()
+    SnipsData.clear()
+    for itemIntent in payload["intent"]:
+        Lexintent={
+            "name": "",
+            "version": "1",
+            "fulfillmentActivity": {"type": "ReturnIntent"},
+            "sampleUtterances": [],
+            "slots": [],
+            "conclusionStatement": {
+                "messages": [{
+                        "groupNumber": 1,
+                        "contentType": "PlainText",
+                        "content": ""}]}
+        }
+        DSintent={
+            "name": "",
+            "phrases": [],
+            "response": ""
+        }
+        Snipsintent={
+            "type": "intent",
+            "name": "despedida",
+            "utterances": []
+        }
+        Lexintent["name"]=itemIntent["name"]
+        Lexintent["sampleUtterances"].extend(itemIntent["examples"])
+        Lexintent["content"]=itemIntent["response"]
+        dataBotLex["resource"]["intents"].append(Lexintent)
+
+        DSintent["name"]=itemIntent["name"]
+        DSintent["phrases"].extend(itemIntent["lexemas"])
+        DSintent["response"]=itemIntent["response"]
+        dataBotDS["ResponseData"].append(DSintent)
+
+        Snipsintent["name"]=itemIntent["name"]
+        Snipsintent["utterances"].extend(itemIntent["examples"])
+        SnipsData.append(Snipsintent)
+    files_upload(SnipsData, dataBotDS, dataBotLex)
+    # engine_update(payload)
+    return {
+        "statusCode": 200,
+        "body":{"status": "entrenamiento completo"}
+    }
+
+def getData():
+    SnipsData, dataBotDS, dataBotLex = files_download()
+    #Generador de Json unificado
+    examples=[]
+    jsonUnificado={"intent":[],"entity":[]}
+    print("snips>> ",SnipsData)
+    print("DS>> ",dataBotDS['ResponseData'])
+    print("lex>> ",dataBotLex['resource']["intents"])
+    for item in SnipsData:
+        for itemDS in dataBotDS['ResponseData']:
+            if itemDS['name']==item['name']:
+                for itemL in dataBotLex['resource']["intents"]:
+                    if itemL['name']==item['name']:
+                        examples.extend(item["utterances"])
+                        examples.extend(itemL["sampleUtterances"])
+                        temp={"name":item['name'],"examples":examples,"response":itemDS["response"],"lexemas":itemDS["phrases"]}
+                        print("temp>> ",temp)
+                        jsonUnificado["intent"].append(temp)
+                        examples=[]
+    return {
+        "statusCode": 200,
+        "body":jsonUnificado
+    }
+
+def engine_update(payload):
     # print("THE BUCKET NAME IS: " + bucket_name)
     # yaml_path = "/tmp/"+ event['bot'] + ".yaml"
     # json_path = "/tmp/"+ event['bot'] + ".json"
@@ -43,74 +148,6 @@ def lambda_handler(event, context):
     # trained_model = nlu_engine.to_byte_array()
     # print("guardando modelo")
     # s3.meta.client.put_object(Bucket=bucket_name,Key=event['bot'] + "_model.json",Body=trained_model)
-    return result
-
-def trainer(payload):
-    #Snips
-    SnipsData=[]
-    s3.meta.client.download_file(bucket_name,bot_name+'.yaml',"/tmp/"+bot_name+'.yaml')
-    with open("/tmp/"+bot_name+'.yaml') as f:
-        for data in yaml.load_all(f, Loader=Loader):
-            SnipsData.append(data)
-    #DistanciaSemantica
-    s3.meta.client.download_file(bucket_name,bot_data_file,"/tmp/"+bot_data_file)
-    f = open("/tmp/"+bot_data_file, "r")
-    content = f.read()
-    dataBotDS = json.loads(content)
-    #Lex
-    s3.meta.client.download_file(bucket_name,bot_name+'.json',"/tmp/"+bot_name+'.json')
-    f = open("/tmp/"+bot_name+'.json', "r")
-    content = f.read()
-    dataBotLex = json.loads(content)
-    print("payload>> ",payload)
-    print("snips>> ",json.dumps(SnipsData))
-    print("DS>> ",json.dumps(dataBotDS))
-    print("lex>> ",json.dumps(dataBotLex))
-    return {
-        "statusCode": 200,
-        "body":{"status": "reentrenado"}
-    }
-
-def getData():
-    #Snips
-    SnipsData=[]
-    s3.meta.client.download_file(bucket_name,bot_name+'.yaml',"/tmp/"+bot_name+'.yaml')
-    with open("/tmp/"+bot_name+'.yaml') as f:
-        for data in yaml.load_all(f, Loader=Loader):
-            SnipsData.append(data)
-    #DistanciaSemantica
-    s3.meta.client.download_file(bucket_name,bot_data_file,"/tmp/"+bot_data_file)
-    f = open("/tmp/"+bot_data_file, "r")
-    content = f.read()
-    dataBotDS = json.loads(content)
-    #Lex
-    s3.meta.client.download_file(bucket_name,bot_name+'.json',"/tmp/"+bot_name+'.json')
-    f = open("/tmp/"+bot_name+'.json', "r")
-    content = f.read()
-    dataBotLex = json.loads(content)
-    #Generador de Json unificado
-    examples=[]
-    jsonUnificado={"intent":[],"entity":[]}
-    print("snips>> ",SnipsData)
-    print("DS>> ",dataBotDS['ResponseData'])
-    print("lex>> ",dataBotLex['resource']["intents"])
-    for item in SnipsData:
-        for itemDS in dataBotDS['ResponseData']:
-            if itemDS['name']==item['name']:
-                for itemL in dataBotLex['resource']["intents"]:
-                    if itemL['name']==item['name']:
-                        examples.extend(item["utterances"])
-                        examples.extend(itemL["sampleUtterances"])
-                        temp={"name":item['name'],"examples":examples,"response":itemDS["response"],"lexemas":itemDS["phrases"]}
-                        print("temp>> ",temp)
-                        jsonUnificado["intent"].append(temp)
-                        examples=[]
-    return {
-        "statusCode": 200,
-        "body":jsonUnificado
-    }
-
-def lex(payload):
     print('>>> ',payload)
     #Extrae el zip del bucket 
     s3Response = s3.get_object(Bucket=payload['bucket'],Key=payload['zip'])
@@ -141,3 +178,4 @@ def lex(payload):
     )
     #Si se desea verificar que el bot está funcionando, se debe agregar un time sleep y volver a consultar con get_bot hasta que el estado sea READY
     return {"NameBot":lexPutBot['name'], "Status":lexPutBot['status'], "Checksum":lexPutBot['checksum'], "Version":lexPutBot['version']}
+
